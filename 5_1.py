4323a6760466101cc71ccb8739d5dec7d0b5ca3a
Andrii-Ishchenko-dp <76679740+Andrii-Ishchenko-dp@users.noreply.github.com>
2021-05-27 15:49:19 +0300

    Update 5_1.py

--- 5_1.py
+++ 5_1.py
@@ -1,3 +1,7 @@
+#Write a Python program to get the largest number from a list of random numbers with the length of 10
+#Constraints: use only while loop and random module to generate numbers
+
+
 import random
 
 list = []
