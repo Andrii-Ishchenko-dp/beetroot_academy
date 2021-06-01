c34802480c2f9e04d1ccde4b0c4a641b8e6b6c8d
Andrii-Ishchenko-dp <76679740+Andrii-Ishchenko-dp@users.noreply.github.com>
2021-06-01 19:20:04 +0300

    Update 6_3.py

--- 6/6_3.py
+++ 6/6_3.py
@@ -1,10 +1,2 @@
-i = []
-j = []
-for o in range(1,11):
-    i.append(o)
-for p in range(len(i)):
-    j.append(i[p]*i[p])
-i = tuple(i)
-j = tuple(j)
-s = [i, j]
-print(s)
+num = [(i,i*i) for i in range(1,10)]
+print(num)
