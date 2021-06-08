ae359b144bc3be857f70b400042f93a97f8015bf
Andrii-Ishchenko-dp <76679740+Andrii-Ishchenko-dp@users.noreply.github.com>
2021-06-08 15:18:47 +0300

    Update 9_1_2.py

--- 9/9_1_2.py
+++ 9/9_1_2.py
@@ -1,3 +1,12 @@
+#Write a script that creates a new output file called myfile.txt and writes the string "Hello file world!" in it. Then write another script that opens myfile.txt, and reads and prints its contents. Run your two scripts from the system command line. 
+
+#Does the new file show up in the directory where you ran your scripts? 
+
+#What if you add a different directory path to the filename passed to open?
+
+#Note: file write methods do not add newline characters to your strings; add an explicit ‘\n’ at the end of the string if you want to fully terminate the line in the file.
+
+
 with open('myfile.txt') as file:
     text_in_file = file.read()
     print(text_in_file)
