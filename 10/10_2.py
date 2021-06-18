d469d65edb94df0734b02332b9b66e8b307c0cd7
Andrii-Ishchenko-dp <76679740+Andrii-Ishchenko-dp@users.noreply.github.com>
2021-06-18 21:15:52 +0300

    Create 10_2.py

---
+++ 10/10_2.py
@@ -0,0 +1,17 @@
+class Dog():
+    age_factor = 7
+    def __init__(self,age):
+        self.age = int(age)
+    def HumanAge(self):
+        return self.age * Dog.age_factor
+    def init_age(self):
+        if self.age <= 0:
+            raise TypeError('Возраст должен быть больше 0!')
+        else:
+            return self.age
+try:
+    Tommy = Dog(input('Введите возвраст собаки: '))
+    Tommy.init_age()
+    print(Tommy.HumanAge())
+except ValueError:
+    print('Возраст должен быть введён числом!')
