625e247a6b76d628230157de9bd186ea68afd30e
Andrii-Ishchenko-dp <76679740+Andrii-Ishchenko-dp@users.noreply.github.com>
2021-06-18 21:17:16 +0300

    Update 10_2.py

--- 10/10_2.py
+++ 10/10_2.py
@@ -1,3 +1,5 @@
+#Create a class Dog with class attribute `age_factor` equals to 7. Make __init__() which takes values for a dog’s age. Then create a method `human_age` which returns the dog’s age in human equivalent.
+
 class Dog():
     age_factor = 7
     def __init__(self,age):
