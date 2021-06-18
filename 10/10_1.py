e00dcbecf8f08a350cb31a47ce94f99752c8be09
Andrii-Ishchenko-dp <76679740+Andrii-Ishchenko-dp@users.noreply.github.com>
2021-06-18 20:43:52 +0300

    Update 10_1.py

--- 10/10_1.py
+++ 10/10_1.py
@@ -2,10 +2,13 @@ class Person():
     def __init__(self, name, s_name, age):
         self.name = name
         self.s_name = s_name
-        self.age = age
+        self.age =  int(age)
 
     def Hi(self):
         print(f'Привет! Меня зовут {self.name} {self.s_name} и мне {self.age} лет ')
 
-person1= Person('Андрей','Ищенко',25)
-person1.Hi()
+try:
+    person1 = Person(name=input('Name: '), s_name=input('Surname: '), age=input('Age: '))
+    person1.Hi()
+except ValueError:
+    print('Возраст должен быть введён числом!')
