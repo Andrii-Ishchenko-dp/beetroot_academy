d6ce652f55047dc795065966cbdec948d3b91ff1
Andrii-Ishchenko-dp <76679740+Andrii-Ishchenko-dp@users.noreply.github.com>
2021-06-18 21:17:43 +0300

    Update 10_1.py

--- 10/10_1.py
+++ 10/10_1.py
@@ -1,3 +1,5 @@
+#Make a class called Person. Make the __init__() method take firstname, lastname, and age as parameters and add them as attributes. Make another method called talk() which makes prints a greeting from the person containing, for example like this: “Hello, my name is Carl Johnson and I’m 26 years old”.
+
 class Person():
     def __init__(self, name, s_name, age):
         self.name = name
