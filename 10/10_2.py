f47356209b1dd7f6ba833c41efe6c0644a8ef6b5
Andrii-Ishchenko-dp <76679740+Andrii-Ishchenko-dp@users.noreply.github.com>
2021-06-19 14:07:24 +0300

    Update 10_2.py

--- 10/10_2.py
+++ 10/10_2.py
@@ -1,5 +1,3 @@
-#Create a class Dog with class attribute `age_factor` equals to 7. Make __init__() which takes values for a dog’s age. Then create a method `human_age` which returns the dog’s age in human equivalent.
-
 class Dog():
     age_factor = 7
     def __init__(self,age):
@@ -17,3 +15,5 @@ try:
     print(Tommy.HumanAge())
 except ValueError:
     print('Возраст должен быть введён числом!')
+except TypeError as err_msg:
+    print('Возраст должен быть больше 0!')
