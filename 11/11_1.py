705b198549ae7a5c08856cf4d2647f91fd36dde3
Andrii-Ishchenko-dp <76679740+Andrii-Ishchenko-dp@users.noreply.github.com>
2021-06-29 16:22:32 +0300

    Update 11_1.py

--- 11/11_1.py
+++ 11/11_1.py
@@ -1,4 +1,5 @@
-class Person:
+class Person():
+
     def __init__(self, person_name, person_sex):
         self.person_name = person_name
         self.person_sex = person_sex
@@ -13,15 +14,15 @@ class Person:
 
 class Student(Person):
     def __init__(self, person_name, person_sex, student_lvl):
-        super.__init__(person_name, person_sex)
+        super().__init__(person_name, person_sex)
         self.student_lvl = student_lvl
 
     def __str__(self):
         return self.person_name + ' ' + self.person_sex + ' ' + self.student_lvl
- 
+
 class Teacher(Person):
     def __init__(self, person_name, person_sex, teacher_zarplata):
-        super.__init__(person_name, person_sex)
+        super().__init__(person_name, person_sex)
         self.teacher_zarplata = teacher_zarplata
     def __str__(self):
         return self.person_name + ' ' + self.person_sex + ' ' + self.teacher_zarplata
