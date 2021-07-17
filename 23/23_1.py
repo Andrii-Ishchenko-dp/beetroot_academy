6d44e1863adcc2bc343b88f3e5ff5abc4ce1117d
Andrii-Ishchenko-dp <76679740+Andrii-Ishchenko-dp@users.noreply.github.com>
2021-07-17 09:57:26 +0300

    Update 23_1.py

--- 23/23_1.py
+++ 23/23_1.py
@@ -1,13 +1,11 @@
 from typing import Optional
-def to_power(x: Optional[int, float], exp: int) -> Optional[int, float]:
-    if exp<0:
-        return TypeError
-    while exp>0:
-        x*=x
-        exp-=1
-    return to_power(x,exp)
-try:
-    y = to_power(3,2)
-    print(y)
-except TypeError as msgg:
-    print('Степерь должна быть больше 0')
+def to_power(x, exp):
+    if exp>0:
+        if exp==1:
+            return x
+        else:
+            y = to_power(x,exp-1)*x
+        return y
+    return ValueError('Степень должна быть больше 0')
+
+print(to_power(3,2))
